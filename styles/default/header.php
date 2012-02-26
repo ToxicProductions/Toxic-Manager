190e29528a873be4d6046c6e7e280ff52995b1ac
Dead-i <x10deadi@yahoo.co.uk>
2012-02-26 09:11:31 +0000

    Adding basic styling for the default stlye

---
+++ styles/default/header.php
@@ -0,0 +1,4 @@
+<table class="header" align="center"><tr>
+    <td class="title"><a href="./"><img src="styles/default/images/logo.png"></td>
+    <td class="options">Welcome back, <?=$_SESSION['username']?>! <a href=\"login.php?logout=1\">Logout</a></td>
+</tr></table>
---
+++ styles/default/headerdata.php
@@ -0,0 +1,8 @@
+<html>
+    
+    <head>
+        <title>Toxic-Manager</title>
+        <link href="styles/default/style.css" rel="stylesheet" type="text/css">
+    </head>
+    
+    <body>
