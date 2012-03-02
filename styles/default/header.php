14a597d7bef83cf0dbb6f05d70eff104ef1c3a8a
GtoXic <queries@toxic-productions.com>
2012-03-02 14:56:05 -0700

    Fixing header bug

--- styles/default/header.php
+++ styles/default/header.php
@@ -1,6 +1,6 @@
 <table class="header" align="center"><tr>
     <td class="title"><a href="./"><img src="styles/default/images/logo.png"></td>
-    <td class="options">Welcome back, <?=$_SESSION['username']?>! <a href=\"login.php?logout=1\">Logout</a></td>
+    <td class="options">Welcome back, <?=$_SESSION['username']?>! <a href="login.php?logout=1">Logout</a></td>
 </tr></table>
 
 <table class="main" align="center" cellspacing="0"><tr>
